3604f37465f9e176e56508977768fbe6abe1ef09
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-02 21:21:42 -0400

    Creando el contexto.

---
+++ RegistroJugadores/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using RegistroJugadores.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroJugadores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options){ }
+        public DbSet<Jugadores> Jugadores { get; set;}
+    }
+}
