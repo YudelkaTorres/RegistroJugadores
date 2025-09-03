b6e2e1ba883c6c5b9062ca520254b24030c243ad
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-02 21:25:11 -0400

    Creando el modelo.

--- RegistroJugadores/Models/Jugadores.cs
+++ RegistroJugadores/Models/Jugadores.cs
@@ -4,5 +4,14 @@ namespace RegistroJugadores.Models
 {
     public class Jugadores
     {
+        [Key]
+        public int JugadorId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombres { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Por favor digitar la cantidad de partidas.")]
+        [Range(0, int.MaxValue, ErrorMessage ="La cantidad de partidas debe ser un número positivo")]
+        public int Partidas { get; set; }
     }
 }
