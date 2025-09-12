afba61def149f521dca3570e5c0e7a52ef25e2df
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-11 21:26:42 -0400

    Agregando DbSet y configuración del contexto para Partidas.

--- RegistroJugadores/DAL/Contexto.cs
+++ RegistroJugadores/DAL/Contexto.cs
@@ -1,11 +1,37 @@
 ﻿using RegistroJugadores.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace RegistroJugadores.DAL
+namespace RegistroJugadores.DAL;
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
-    {
-        public Contexto(DbContextOptions<Contexto> options) : base(options){ }
-        public DbSet<Jugadores> Jugadores { get; set;}
-    }
+    public Contexto(DbContextOptions<Contexto> options) : base(options){ }
+    public DbSet<Jugadores> Jugadores { get; set;}
+     public DbSet<Partidas> Partidas { get; set;}
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+     {
+        modelBuilder.Entity<Partidas>()
+           .HasOne(partida => partida.Jugador1)
+           .WithMany()
+           .HasForeignKey(Partidas => Partidas.Jugador1Id)
+           .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partidas>()
+           .HasOne(p => p.Jugador2)
+           .WithMany()
+           .HasForeignKey(p => p.Jugador2Id)
+           .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partidas>()
+           .HasOne(p => p.Ganador)
+           .WithMany()
+           .HasForeignKey(p => p.GanadorId)
+           .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partidas>()
+            .HasOne(p => p.TurnoJugador)
+            .WithMany()
+            .HasForeignKey(p => p.TurnoJugadorId)
+            .OnDelete(DeleteBehavior.Restrict);
+     }
 }
+
