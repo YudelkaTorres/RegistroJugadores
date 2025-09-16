2e03223de67ec7a9fadcef4d1ba1e6d77d5a1126
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-09-15 20:32:55 -0400

    Creando una migración.

---
+++ RegistroJugadores/Migrations/20250916002543_Third.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroJugadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class Third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Partidas",
+                table: "Jugadores",
+                newName: "Victorias");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Victorias",
+                table: "Jugadores",
+                newName: "Partidas");
+        }
+    }
+}
