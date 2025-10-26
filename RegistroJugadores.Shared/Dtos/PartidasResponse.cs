1a157fd63e28e1e0f372f670c685e151f9733545
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-26 16:35:13 -0400

    Creando los Dtos Movimientos y Partidas.

---
+++ RegistroJugadores.Shared/Dtos/PartidasResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroJugadores.Shared.Dtos;
+public record PartidasResponse(
+    int Jugador1Id,
+    int? Jugador2Id
+    );
