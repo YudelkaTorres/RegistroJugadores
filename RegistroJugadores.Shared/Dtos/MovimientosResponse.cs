8f96291235ecf8b652bf26fe5ad625ce3feba00b
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-27 13:44:02 -0400

    Creando la class Library shared y sus respectivos Dtos

---
+++ RegistroJugadores.Shared/Dtos/MovimientosResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroJugadores.Shared.Dtos;
+public record MovimientosResponse(
+    int PartidadId,
+    int PosicionFila,
+    int PosicionColumna,
+    string Jugador
+    );
