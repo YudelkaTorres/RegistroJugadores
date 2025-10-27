c5ec0fda0ff6b93c901ec7c5b475fa965a2cbbb1
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-27 13:39:45 -0400

    Creando el services del Api.

---
+++ RegistroJugadores.BlazorWasm/Services/IMovimientosApiService.cs
@@ -0,0 +1,8 @@
+﻿using RegistroJugadores.Shared;
+using RegistroJugadores.Shared.Dtos;
+namespace RegistroJugadores.BlazorWasm.Services;
+public interface IMovimientosApiService
+{
+    Task<Resource<List<MovimientosResponse>>> GetMovimientosAsync(int partidaId);
+    Task<Resource<MovimientosResponse>> PostMovimiento(int partidaId, int posicionFila, int posicionColumna, string jugador);
+}
