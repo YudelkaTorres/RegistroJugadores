8aecbd4884256a37710bf19d793aa91578889aa0
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-11-01 00:36:41 -0400

    Eliminando BlazorWasm y Shared.

--- RegistroJugadores.BlazorWasm/Services/IMovimientosApiService.cs
+++
@@ -1,8 +0,0 @@
-﻿using RegistroJugadores.Shared;
-using RegistroJugadores.Shared.Dtos;
-namespace RegistroJugadores.BlazorWasm.Services;
-public interface IMovimientosApiService
-{
-    Task<Resource<List<MovimientosResponse>>> GetMovimientosAsync(int partidaId);
-    Task<Resource<MovimientosResponse>> PostMovimiento(int partidaId, int posicionFila, int posicionColumna, string jugador);
-}
